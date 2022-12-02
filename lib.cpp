bd398db2feacbc1165f03d58f3952afebb42a5fe
andreavallebona <115356293+andreavallebona@users.noreply.github.com>
2022-12-02 22:04:07 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+int primo(int a, int b){
+    bool x=true;
 
+    if(b!=1){
+        if((a%b) !=0){
+            b--;
+            x= primo(a,b);
+        }else{
+            x=false;
+        }
+    }else{
+        x=true;
+    }
+    return x;
+}
