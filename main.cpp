0fc7bdc3282050995c62e2bdf5ed7d75743c9e29
andreavallebona <115356293+andreavallebona@users.noreply.github.com>
2022-12-02 22:05:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "primaLibreria.h"13
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    int a=0; cin >>a;
+    int b=(a-1);
+
+    if(primo(a,b) == true){
+        cout << "numero primo" << endl;
+    }else if(primo(a,b) == false){
+        cout << "numero non primo" << endl;
+    }
+    return 0;
 }
